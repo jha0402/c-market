af8f1fd4774cc88ee60a7fae32953b74f2375125
jha0402 <67306416+jha0402@users.noreply.github.com>
2022-02-04 18:11:05 +0900

    Testing tailwind, modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,41 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
     return (
-        <div className='bg-slate-400 py-20 px-20 grid gap-10 min-h-screen'>
+        // JIT: Just in time compiler
+        // Before there was a purging process that cleans up the unnecessary css for production and modifiers could not be used together
+        //
+        <div className='bg-slate-400 py-20 px-20 grid gap-10 min-h-screen xl:place-content-center lg:grid-cols-2 xl:grid-cols-3'>
             {/* Checkout card */}
-            <div className='bg-white p-6 rounded-3xl shadow-xl'>
-                <span className='font-semibold text-3xl'>Select Item</span>
+            {/* Responsive modifiers: sm - 640px, md - 768px, lg - 1024px, xl - 1280px, 2xl - 1546px 
+                landscape:, portrait: - orientation based style
+            */}
+            {/* Dark mode modifier: dark:
+                Dark mode config(tailwind.config.js) - if set to "media" listens to browser
+                if set to "class" sets only to elements with classname dark
+            */}
+            <div className='bg-white p-6 rounded-3xl shadow-xl flex flex-col justify-between dark:bg-black dark:text-white'>
+                <span className='font-semibold text-3xl dark:text-white'>Select Item</span>
+                {/* Modifiers practiced - first:, last: only:, odd:, even:, empty: */}
                 <div className='flex justify-between my-2'>
-                    <span className='text-gray-500'>Grey chair</span>
+                    <span className='text-gray-500 dark:text-gray-50'>Grey chair</span>
                     <span className='font-semibold'>$19</span>
                 </div>
-                <div className='flex justify-between'>
-                    <span className='text-gray-500'>Tooly Table</span>
-                    <span className='font-semibold'>$8</span>
+                <div className='flex justify-between my-2'>
+                    <span className='text-gray-500 dark:text-gray-50'>Grey chair</span>
+                    <span className='font-semibold'>$19</span>
                 </div>
                 <div className='flex justify-between mt-2 pt-2 border-t-2 border-dashed'>
                     <span>Total</span>
                     <span className='font-semibold'>$8</span>
                 </div>
-                <button className='mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto block hover:bg-teal-500'>
+                <button className='mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto block hover:bg-teal-500 dark:bg-white dark:text-black'>
                     Checkout
                 </button>
             </div>
             {/* Profile card */}
-            <div className='bg-white overflow-hidden rounded-3xl shadow-xl'>
-                <div className='bg-blue-500 p-6 pb-14'>
+            {/* Practicing group modifier: Assign what is the 'group'; the card container */}
+            <div className='bg-white overflow-hidden rounded-3xl shadow-xl group'>
+                <div className='portrait:bg-indigo-600 landscape:bg-teal-500 p-6 pb-14 xl:pb-40'>
                     <span className='text-white text-2xl'>Profile</span>
                 </div>
                 <div className='rounded-3xl p-6 relative -top-5 bg-white'>
@@ -33,7 +45,8 @@ const Home: NextPage = () => {
                             <span className='text-sm text-gray-500'>Orders</span>
                             <span className='font-medium'>340</span>
                         </div>
-                        <div className='h-24 w-24 bg-red-400 rounded-full'></div>
+                        {/* When the group is on hover, change the background of this element */}
+                        <div className='h-24 w-24 bg-zinc-300 rounded-full group-hover:bg-red-300 transition-colors'></div>
                         <div className='flex flex-col items-center'>
                             <span className='text-sm text-gray-500'>Spent</span>
                             <span className='font-medium'>$340</span>
@@ -46,7 +59,7 @@ const Home: NextPage = () => {
                 </div>
             </div>
             {/* Product card */}
-            <div className='bg-white p-10 rounded-3xl shadow-xl'>
+            <div className='bg-white p-10 rounded-3xl shadow-xl lg:col-span-2 xl:col-span-1'>
                 <div className='flex mb-5 justify-between items-center'>
                     <span>⬅️</span>
                     <div className='space-x-3'>
@@ -81,6 +94,42 @@ const Home: NextPage = () => {
                         </button>
                     </div>
                 </div>
+            </div>
+            {/* Form */}
+            {/* Usable modifiers: focus-within:, required:, valid:, invalid:, placeholder:, placeholder-shown: */}
+            {/* Peer modifier: The style of an element depending on the state of another (className= peer) 
+                peer element has to come first
+            */}
+            <form className='flex flex-col space-y-2 bg-blue-500 p-5 focus-within:bg-blue-200' action=''>
+                <input
+                    type='text'
+                    placeholder='Username'
+                    required
+                    className='peer required:border-2 border-yellow-500 invalid:bg-red-500'
+                />
+                <span className='hidden peer-invalid:block text-red-500'>This input is invalid</span>
+                <input type='password' />
+                <input type='submit' />
+            </form>
+
+            {/* More modifiers: */}
+            {/* selection:, open:, marker:, file:, file:hover:, first-letter: */}
+            <div className='flex flex-col space-y-2 p-5'>
+                <details className='select-none open:text-white open:bg-indigo-400'>
+                    <summary className='cursor-pointer'>Summary tag</summary>
+                    <span className='selection:bg-indigo-500'>Detail content</span>
+                </details>
+                <ul className='list-disc marker:text-teal-500'>
+                    <li>Hello</li>
+                    <li>Hi</li>
+                </ul>
+                <input
+                    type='file'
+                    className='file:cursor-pointer file:hover:text-blue-500 file:transition-colors file:border-0 file:rounded-md'
+                />
+                <p className='first-letter:text-7xl first-letter:hover:text-blue-500'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
             </div>
         </div>
     );
