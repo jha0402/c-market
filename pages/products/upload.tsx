4bd047ce8a8bc7e5199b28b88bd73e12986facb7
jha0402 <67306416+jha0402@users.noreply.github.com>
2022-02-09 17:45:33 +0900

    Add live create page, Modify product upload page

--- pages/products/upload.tsx
+++ pages/products/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 
 const Upload: NextPage = () => {
     return (
-        <div className='px-4 py-16'>
+        <div className='px-4 space-y-5 py-10'>
             <div>
                 <div className='w-full cursor-pointer flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md text-gray-600 hover:text-orange-500 hover:border-orange-500'>
                     <label>
@@ -24,7 +24,20 @@ const Upload: NextPage = () => {
                     </label>
                 </div>
             </div>
-            <div className='my-5'>
+            <div>
+                <label className='mb-1 block text-sm font-medium text-gray-700' htmlFor='name'>
+                    Name
+                </label>
+                <div className='rounded-md relative flex  items-center shadow-sm'>
+                    <input
+                        id='name'
+                        type='email'
+                        className='appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500'
+                        required
+                    />
+                </div>
+            </div>
+            <div>
                 <label htmlFor='price' className='mb-1 block text-sm font-medium text-gray-700'>
                     Price
                 </label>
@@ -44,14 +57,17 @@ const Upload: NextPage = () => {
                 </div>
             </div>
             <div>
-                <label className='mb-1 block text-sm font-medium text-gray-700'>Description</label>
+                <label htmlFor='description' className='mb-1 block text-sm font-medium text-gray-700'>
+                    Description
+                </label>
 
                 <textarea
+                    id='description'
                     className='mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500'
                     rows={4}
                 />
             </div>
-            <button className='w-full mt-5 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none'>
+            <button className='w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none'>
                 Upload product
             </button>
         </div>
