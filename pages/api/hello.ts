3fce3303745c875a307cda48fabc98af5f39f2de
jha0402 <67306416+jha0402@users.noreply.github.com>
2022-02-10 23:22:53 +0900

    Test api planetscale db create

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import client from '../../libs/client';
 
 type Data = {
-  name: string
-}
+    name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    await client.user.create({
+        data: {
+            email: 'hi',
+            name: 'hi',
+        },
+    });
+    res.status(200).json({ name: 'John Doe' });
 }
