c048cdb0bfdc115085c167ef74c17728893cda3d
jha0402 <67306416+jha0402@users.noreply.github.com>
2022-02-20 21:29:23 +0900

    Fix useMutation bug

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -6,6 +6,7 @@ interface UseMutationState<T> {
     error?: object;
 }
 type UseMutationResult<T> = [(data: any) => void, UseMutationState<T>];
+
 export default function useMutation<T = any>(url: string): UseMutationResult<T> {
     const [state, setState] = useState<UseMutationState<T>>({
         loading: false,
@@ -22,9 +23,8 @@ export default function useMutation<T = any>(url: string): UseMutationResult<T> 
             body: JSON.stringify(data),
         })
             .then((response) => response.json().catch(() => {}))
-            .then((data) => setState((prev) => ({ ...prev, data })))
-            .catch((error) => setState((prev) => ({ ...prev, error })))
-            .finally(() => setState((prev) => ({ ...prev, loading: true })));
+            .then((data) => setState((prev) => ({ ...prev, data, loading: false })))
+            .catch((error) => setState((prev) => ({ ...prev, error, loading: false })));
     }
     return [mutation, { ...state }];
 }
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -45,6 +45,7 @@ const Enter: NextPage = () => {
         confirmToken(validForm);
     };
     console.log(data);
+    console.log(tokenLoading);
     return (
         <div className='mt-16 px-4'>
             <h3 className='text-3xl font-bold text-center'>Enter to C-Market</h3>
