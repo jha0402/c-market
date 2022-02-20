1eb6ee5c452508c2a127e06449c85b32758bd82c
jha0402 <67306416+jha0402@users.noreply.github.com>
2022-02-20 20:22:58 +0900

    Add serverless session feature

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -2,11 +2,24 @@
 import client from '@libs/server/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import withHandler, { ResponseType } from '@libs/server/withHandler';
+import { withIronSessionApiRoute } from 'iron-session/next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
     const { token } = req.body;
-    console.log(token);
+    const auth = await client.token.findUnique({
+        where: {
+            payload: token,
+        },
+    });
+    if (!auth) res.status(404).end();
+    req.session.user = {
+        id: auth?.userId,
+    };
+    await req.session.save();
     res.status(200).end();
 }
 
-export default withHandler('POST', handler);
+export default withIronSessionApiRoute(withHandler('POST', handler), {
+    cookieName: 'csession',
+    password: 'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf',
+});
