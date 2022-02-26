890a5b3f51ec6b78bac6850fccc83e853c0738a6
jha0402 <67306416+jha0402@users.noreply.github.com>
2022-02-26 16:32:55 +0900

    Add product detail api

---
+++ pages/api/products/[id].ts
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import client from '@libs/server/client';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import { withApiSession } from '@libs/server/withSession';
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+    const { id } = req.query;
+    const product = await client.product.findUnique({
+        where: {
+            id: +id.toString(),
+        },
+        include: {
+            user: {
+                select: {
+                    id: true,
+                    name: true,
+                    avatar: true,
+                },
+            },
+        },
+    });
+    res.json({ ok: true, product });
+}
+
+export default withApiSession(
+    withHandler({
+        methods: ['GET'],
+        handler,
+    })
+);
