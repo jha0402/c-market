0c2d6f5a0fc5aa42b223b31b36aebf6847795648
jha0402 <67306416+jha0402@users.noreply.github.com>
2022-02-27 20:12:10 +0900

    Add wishlist api

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -5,7 +5,10 @@ import withHandler, { ResponseType } from '@libs/server/withHandler';
 import { withApiSession } from '@libs/server/withSession';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
-    const { id } = req.query;
+    const {
+        query: { id },
+        session: { user },
+    } = req;
     const product = await client.product.findUnique({
         where: {
             id: +id.toString(),
@@ -35,8 +38,18 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
             },
         },
     });
-
-    res.json({ ok: true, product, relatedProducts });
+    const isLiked = Boolean(
+        await client.fav.findFirst({
+            where: {
+                productId: product?.id,
+                userId: user?.id,
+            },
+            select: {
+                id: true,
+            },
+        })
+    );
+    res.json({ ok: true, product, relatedProducts, isLiked });
 }
 
 export default withApiSession(
